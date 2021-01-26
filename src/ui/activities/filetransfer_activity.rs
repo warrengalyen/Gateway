d036229ba1e587d393e5a915a6206a9401c6cec1
Warren Galyen <wgalyen@gmail.com>
2021-01-26 04:49:55 -0800

    Draw popup area

--- src/ui/activities/filetransfer_activity.rs
+++ src/ui/activities/filetransfer_activity.rs
@@ -1569,6 +1569,34 @@ impl FileTransferActivity {
             .start_corner(Corner::BottomLeft)
     }
 
+    /// ### draw_popup_area
+    ///
+    /// Draw popup area
+    fn draw_popup_area(&self, area: Rect) -> Rect {
+        let popup_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(
+                [
+                    Constraint::Percentage((80) / 2),
+                    Constraint::Percentage(20),
+                    Constraint::Percentage((80) / 2),
+                ]
+                .as_ref(),
+            )
+            .split(area);
+        Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(
+                [
+                    Constraint::Percentage((80) / 2),
+                    Constraint::Percentage(20),
+                    Constraint::Percentage((80) / 2),
+                ]
+                .as_ref(),
+            )
+            .split(popup_layout[1])[1]
+    }
+
     /// ### draw_footer
     ///
     /// Draw authentication page footer
